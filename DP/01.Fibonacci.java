5067b85913c15e31f0bdef7a77928cb9a5695476
Tamanna Verma <tamannav01@gmail.com>
2022-04-07 15:05:38 +0530

    Create 01.Fibonacci.java

---
+++ DP/01.Fibonacci.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) throws Exception
+{
+    Scanner scan=new Scanner(System.in);
+   int n=scan.nextInt();
+   int dp[]=new int [n+1];
+   
+   dp[0]=0;
+   dp[1]=1;
+   
+  for(int i=2;i<dp.length;i++)
+  {
+      dp[i]=dp[i-1]+dp[i-2];
+  }
+  System.out.println(dp[n]);
+  
+ }
+
+}
